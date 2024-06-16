19a80d897bf196fdd3f272328af7d4c81442d30a
Pasha Altapov <2004pablo2004@gmail.com>
2024-06-16 23:11:24 +0300

    fix edit user

--- app.js
+++ app.js
@@ -192,3 +192,15 @@ app.patch('/dances/:id', async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+
+app.patch('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
